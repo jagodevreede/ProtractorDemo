8d0d26c3adf4b70da8d8d8df60d69ea80bc12e48
Jago de Vreede <jago.devreede@gmail.com>
2016-02-28 12:45:00 +0100

    Fixed bad class path warning added source maps to sass

--- org.demo.protractor.ui/tasks/sass.js
+++ org.demo.protractor.ui/tasks/sass.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var config = require('../gulp.config')();
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
     return gulp.src(config.assetsPath.styles + 'main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.assetsPath.styles));
 });
  
