62bd4713c9ba92f0d9a8a9cd2f327a3871c31407
Jago de Vreede <jago.devreede@gmail.com>
2016-03-12 22:18:39 +0100

    Removed console.log

--- org.demo.protractor.ui/app/blocks/CORSBrowserXHR.ts
+++ org.demo.protractor.ui/app/blocks/CORSBrowserXHR.ts
@@ -4,7 +4,7 @@ import {Injectable} from 'angular2/core';
 @Injectable()
 export class CORSBrowserXHR extends BrowserXhr {
     build(): any {
-        console.log("cros");
+        // Should not be needed anymore in next beta
         let xhr: any = super.build();
         xhr.withCredentials = true;
         return xhr;
