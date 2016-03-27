398d9f9a13bee1e924c4cc9a91ac1b73a664dedb
Robert Zwerus <arzie@dds.nl>
2016-03-27 20:33:39 +0200

    Implement a bit of 05.todo.spec

--- org.demo.protractor.ui/test/e2e/page/TodoPage.ts
+++ org.demo.protractor.ui/test/e2e/page/TodoPage.ts
@@ -1,7 +1,12 @@
+import webdriver = require('selenium-webdriver');
 import BasePage = require('./_basePage');
 
 class TodoPage extends BasePage {
     addList = element(by.id('addNewList'));
+    todoLists = element.all(by.repeater('todoList in todolistCtrl.todos'));
+    todoItemsLocator = by.repeater('item in todoList.list');
+    todoItemDescriptionLocator = by.id('todo');
+    addButtonLocator = by.css('button.btn-primary');
 
     open() {
         browser.get('#/todo');
@@ -11,6 +16,24 @@ class TodoPage extends BasePage {
         return this.addList.isPresent();
     }
 
+    getTodoLists() {
+        return this.todoLists;
+    }
+
+    getTodos(todoListIndex) {
+        return this.todoLists.get(todoListIndex).all(this.todoItemsLocator);
+    }
+
+    getTodo(todoListIndex, todoItemIndex) {
+        return this.todoLists.get(todoListIndex).all(this.todoItemsLocator).get(todoItemIndex).element(by.css('.description')).getText();
+    }
+
+    addTodoItem(todoListIndex, todoItemDescription) {
+        this.todoLists.get(todoListIndex)
+            .element(this.todoItemDescriptionLocator)
+            .sendKeys(webdriver.Key.CONTROL, 'a', webdriver.Key.NULL, todoItemDescription);
+        return this.todoLists.get(todoListIndex).element(this.addButtonLocator).click();
+    }
 }
 
 export = TodoPage;
--- org.demo.protractor.ui/test/e2e/uc/05.todo.spec.ts
+++ org.demo.protractor.ui/test/e2e/uc/05.todo.spec.ts
@@ -7,4 +7,15 @@ describe('Todo page', () => {
         todo.open();
         expect(todo.isOpen()).toBeTruthy();
     });
+
+    it('should have one todo list with zero items', () => {
+        expect(todo.getTodoLists().count()).toEqual(1);
+        expect(todo.getTodos(0).count()).toEqual(0);
+    });
+
+    it('should be able to add a todo item', () => {
+        todo.addTodoItem(0, 'test todo');
+        expect(todo.getTodos(0).count()).toEqual(1);
+        expect(todo.getTodo(0, 0)).toEqual('test todo');
+    });
 });
