6f17a0d2de7f726ac897a49840b092494ac2f4ee
Robert Zwerus <robert.zwerus@luminis.eu>
2016-04-13 17:05:11 +0200

    Remove typings stuff.

--- org.demo.protractor.ui/tasks/typescript.js
+++ org.demo.protractor.ui/tasks/typescript.js
@@ -9,8 +9,6 @@ var path = require('path');
 var tsProject = ts.createProject(config.root + 'tsconfig.json');
 var e2eTsProject = ts.createProject(config.root + 'tsconfig-e2e.json');
 var typingFiles = [
-    'typings/main.d.ts',
-    'typings/main/*.d.ts'
 ];
 var tsUnitFiles = [].concat(config.tsTestFiles.unit);
 var tsE2EFiles = [].concat(config.tsTestFiles.e2e, config.tsTestFiles.page);
